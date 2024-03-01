904ed2656d095addce9a87c5bd46fc2b384b734b
adeeburrahman11 <adeeburrahman9021@gmail.com>
2024-03-01 23:02:09 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,37 @@
-runApp();
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        // backgroundColor: Color.fromARGB(255, 230, 230, 20),
+        body: GradientContainer(),
+      ),
+    ),
+  );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 248, 248, 105),
+            Color.fromARGB(255, 240, 240, 0),
+          ],
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Adeebur Rahman',
+          style: TextStyle(
+            color: Color.fromARGB(255, 0, 0, 0),
+            fontSize: 28,
+          ),
+        ),
+      ),
+    );
+  }
+}
